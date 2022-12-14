3bb438c8e534f30322592c39843f2e571d7c3fa5
Natalia <nataliallcoelho@gmail.com>
2022-12-14 16:10:58 -0300

    componente card para diagrama

--- src/pages/dashboard/documents/Documents.jsx
+++ src/pages/dashboard/documents/Documents.jsx
@@ -1,8 +1,21 @@
+import DiagramCard from "../../../components/DiagramCard";
 
 function Documents() {
     return (
-        <div className="container">
+        <div className="flex-fill h-100">
+            {/* NavBar aqui */}
             <h3>Documentos</h3>
+
+            {/* Tabs aqui, dentro das tabs o container */}
+
+            <div className="container">
+
+                <div className="row">
+                    <div className="col-12 col-md-4 col-lg-3">
+                        <DiagramCard/>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
