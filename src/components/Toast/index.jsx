a0d629555981c6f064d6e78e0337b0c1b327319f
caio <caiof557@gmail.com>
2024-05-09 10:10:17 -0300

    cores de pop-up trocadas

--- src/components/Toast/index.jsx
+++ src/components/Toast/index.jsx
@@ -7,13 +7,13 @@ export function Toast(status, text, icons) {
     const MySwal = withReactContent(Swal)
 
     let bgColors = {
-        'success': '#4ECB7112',
-        'error': '#EA5B5B0D'
+        'success': '#007D32',
+        'error': '#AA1010'
     }
 
     let colors = {
-        'success': '#4ECB71',
-        'error': '#EA5B5B'
+        'success': '#FFFFFF',
+        'error': '#FFFFFF'
     }
     
     MySwal.fire({
