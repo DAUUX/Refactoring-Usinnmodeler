04d6faf659d7fcb573d6c26ced85b4a43fa329ee
Mayronn Gomes <mayronngomes@alu.ufc.br>
2024-06-27 13:47:59 -0300

    Adicionando uma data mínima

--- src/pages/register/Register.jsx
+++ src/pages/register/Register.jsx
@@ -13,7 +13,7 @@ import { useEffect } from "react";
 export default function Register() {
 
 	let DateTenYears = new Date();
-	DateTenYears.setFullYear((new Date).getFullYear() - 10);
+	DateTenYears.setFullYear((new Date()).getFullYear() - 10);
 
 	useEffect(() => {
     document.title = 'Cadastrar - USINN Modeler';
@@ -45,6 +45,7 @@ export default function Register() {
 			birthday: Yup.date()
 				.transform((value, currentValue) => { return moment(currentValue, 'DD/MM/YYYY', true).toDate() })
 				.typeError('Data é inválida')
+				.min(new Date(0, 0, 1), 'Data de nascimento inválida')
 				.max(DateTenYears, 'Data de nascimento inválida')
 				.required('Data de nascimento é obrigatória'),
 			gender: Yup.number().integer('Valor é inválido').min(1, 'Valor é inválido').max(3, 'Valor é inválido').required('Gênero é obrigatório'),
