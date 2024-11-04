386a455d15c1b99555f4ac90d6acfb9aedb68dd6
Antonio Lisboa de Carvalho <104227926+LisboaAnt@users.noreply.github.com>
2024-11-04 09:15:03 -0300

    add / Inicio / Modelos de Diagramas

--- src/pages/dashboard/inicio/Inicio.jsx
+++ src/pages/dashboard/inicio/Inicio.jsx
@@ -1,31 +1,34 @@
 import UserProfile from "../../../components/UserProfile";
 import Documents_inicio from './Documents_inicio'
+import Modelos_documentos from './Modelos_documentos'
 import './style.scss'
 
 function Inicio(){
-    const {resultcardModels, cardModels} = 0;
     const { resultcardRecentes, cardRecentes } = Documents_inicio();
+
+    const {resultcardModels, cardModels} = Modelos_documentos();
     const Data = JSON.parse(localStorage.getItem('user'));
 
     return(        
     
     <div id="inicioPage" className="flex-fill ">
     
-        <nav className="navbar navbar-expand-lg p-3 justify-content-end">     
+        <nav className="navbar navbar-expand-lg p-3 justify-content-end ">    
+                
+                <div className="h3 text-center pt-2 w-100">
+                    <b>Seja bem-vindo(a), {Data.name}!</b>
+                </div> 
+                
                 <div className="d-flex ">
                     <UserProfile />
                 </div>
         </nav>
 
-  
         <nav className="container-fluid pt-0 pt-md-2">
-            <div className="h4 text-center ">
-                <b>Seja bem-vindo(a), {Data.name}!</b>
-            </div>
             {resultcardModels && (
-                <div className="px-md-5">
+                <div className="px-md-0 mt-5">
                     <h3 className="ps-4">Modelos de Diagramas</h3>
-                    <div className="align-items-center">
+                    <div className="">
                         {cardModels}
                     </div>
                 </div>
@@ -47,4 +50,4 @@ function Inicio(){
     )
 }
 
-export default Inicio;
+export default Inicio;
---
+++ src/pages/dashboard/inicio/Modelos_documentos.jsx
@@ -0,0 +1,101 @@
+import { useState, useEffect } from "react";
+import DiagramCard from "../../../components/DiagramCard";
+import Spinner from "../../../components/Spinner";
+import { Toast } from "../../../components/Toast";
+import api from "../../../services/api";
+import { Modal } from "bootstrap";
+import ShareDiagramModal from "../../../components/ShareDiagramModal";
+import RemoveDiagramModal from "../../../components/RemoveDiagramModal";
+import RenameDiagramModal from "../../../components/RenameDiagramModal";
+
+function Modelos_documentos() {
+
+    let [diagrams, setDiagrams] = useState([]);
+    let [loading, setLoading] = useState(true);
+
+    const [selectedId, setSelectedId] = useState(null);
+    
+    async function getDiagrams() {
+        setLoading(true);
+        try{
+            const res = await api.get('/diagrams/recent?limit=4');
+            setDiagrams(res.data.diagrams);
+        } catch(error){
+            Toast('error', error);
+        }
+        setLoading(false);
+    }
+
+    useEffect(()=>{
+        getDiagrams();
+    },[])
+
+    function callShareDiagramModal(id) {
+        setSelectedId(id)
+
+        const modal = new Modal('#ShareDiagramModal')          
+        modal.show();
+    }
+
+    function callRemoveDiagramModal(id) {
+        setSelectedId(id)
+
+        const modal = new Modal('#RemoveDiagramModal')          
+        modal.show();
+    }
+
+    function callRenameDiagramModal(id) {
+        setSelectedId(id)
+
+        const modal = new Modal('#RenameDiagramModal')          
+        modal.show();
+    }
+    
+    // Variável com os quatro primeiros diagramas
+    const resultcardModels = diagrams.length !== 0;
+
+    const cardModels = (
+        <div className="container-fluid px-4 ">
+            <div className="row ">
+                {
+                    loading && (
+                        <div className="col-12 d-flex mt-5 justify-content-center">
+                            <Spinner className="spinner-border me-2" isLoading={loading} />
+                        </div>
+                    )
+                }
+                {
+                    diagrams.length > 0 && !loading && (
+                        diagrams.map((diagram) => (
+                            <div key={diagram.id} className="col-12 col-md-4 col-lg-3 mb-3" >
+                                <DiagramCard
+                                    id={diagram.id}
+                                    name={diagram.name}
+                                    lastModified={diagram.updatedAt}
+                                    userId={diagram.user_id}
+                                    thumbnail={diagram.diagram_svg}
+                                    onShareDiagram={(id) => callShareDiagramModal(id)}
+                                    onRemoveDiagram={(id) => callRemoveDiagramModal(id)}
+                                    onRenameDiagram={(id) => callRenameDiagramModal(id)}
+                                    favorited={diagram.favorite}
+                                    onDiagramFavorited={() => { }}
+                                />
+                            </div>
+                        ))
+                    )
+                }
+                {
+                    (<></>)
+                }
+            </div>
+            <ShareDiagramModal id="ShareDiagramModal" diagram_id={selectedId} />
+            <RemoveDiagramModal id="RemoveDiagramModal" diagram_id={selectedId} onDiagramRemoved={() => getDiagrams()} />
+            <RenameDiagramModal id="RenameDiagramModal" diagram_id={selectedId} onDiagramRenamed={() => getDiagrams()} />
+        </div>
+    );
+
+    // Retorne as variáveis result e card dentro de um objeto
+    return { resultcardModels, cardModels };
+}
+
+export default Modelos_documentos;
